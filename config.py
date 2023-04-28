38cf52a5dc770fcb0f7b88dfa1ea4814a3732fca
shyamsar30 <87701039+shyamsar30@users.noreply.github.com>
2023-04-28 17:21:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     ELASTICSEARCH_HOST_NAME = "https://192.168.1.9:9200/"
-    SSL_ASSERT_FINGERPRINT = 'b5dca61e04fc664708ba468556d975ce8e5d157f8852491e276cf5fb4cfb58b6'
+    SSL_ASSERT_FINGERPRINT = 'SSL_ASSERT_FINGERPRINT_HERE'
     USERNAME = 'shyam'
-    PASSWORD = 'shyam@elastic'
+    PASSWORD = 'ASKSHYAMFORPASSWORD'
     ELASTIC_INDEX_NAME = 'products_v2'
     DEFAULT_FETCH_SIZE = 20
     AUTO_COMPLETE_FIELDS = ['category5_name', 'store_name', 'shopify_subcategory', 'title']
